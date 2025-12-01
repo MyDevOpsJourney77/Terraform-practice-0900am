106b42e057d125f7c86eaf01fd3565aee60f65f7
MyDevOpsJourney77 <sonawanekundan777@gmail.com>
2025-12-01 18:29:36 +0530

    Delete Day-3-Statefile directory

--- Day-3-Statefile/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "597688116998"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-  }
-}
--- Day-3-Statefile/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "dev" {
-    ami = "ami-00ca32bbc84273381"
-    instance_type = "t2.micro"
-    tags = {
-      Name = "devtest"   
-       }
-  
-}
